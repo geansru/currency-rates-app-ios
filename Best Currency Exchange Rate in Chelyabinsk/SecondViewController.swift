65de4641e2452035f76a3e5e462deaa15513dc35
Dmitry Roytman <d@geans.ru>
2015-09-15 13:33:11 +0500

    Initial Commit

---
+++ Best Currency Exchange Rate in Chelyabinsk/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Best Currency Exchange Rate in Chelyabinsk
+//
+//  Created by Dmitriy Roytman on 15.09.15.
+//  Copyright (c) 2015 Dmitriy Roytman. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
